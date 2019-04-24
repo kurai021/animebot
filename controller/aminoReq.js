d195de79cbe94d907ac8143f4de784138d2f766c
Richard Armuelles <admin@kurai021.com>
2019-04-24 11:06:37 -0500

    new features (random manga and anime)

--- controller/aminoReq.js
+++ controller/aminoReq.js
@@ -34,7 +34,11 @@ let poller = new Poller(5000);
                 .msg
             let mangaMatch = message.match(/\/getManga (.*)/)
             let animeMatch = message.match(/\/getAnime (.*)/)
+            let randomAnimeMatch = message.match(/\/getRandomAnime (.*)/)
+            let randomMangaMatch = message.match(/\/getRandomManga (.*)/)
             let helpMatch = message.match(/\/help/)
+
+            /* orden help */
 
             if (helpMatch != null && lastMessage.messages[0].author.uid != myProfile.account.uid) {
 
@@ -42,7 +46,7 @@ let poller = new Poller(5000);
                     auth.amino.community,
                     receiver,
                     `
-                    /help: obtiene estas instrucciones
+                    /help: Obtiene estas instrucciones
                     
 /getManga título: Obtiene información de un manga específico (título nativo, romaji, imagen, inicio y fin de publicación, capítulos, volumenes y descripción).
 
@@ -51,12 +55,20 @@ Ejemplo: /getManga death note
 /getAnime título: Obtiene información de un anime específico (título nativo, romaji, imagen, inicio y fin de publicación, episodios, duración aproximada y descripción).
 
 Ejemplo: /getAnime evangelion
+
+/getRandomAnime categoría: Obtiene información de un anime al azar
+
+/getRandomManga categoría: Obtiene información de un manga al azar
+
+Las categorías definidas en ambos casos son: Action, Adventure, Comedy, Drama, Ecchi, Fantasy, Horror, Mahou Shoujo, Mecha, Music, Mystery, Psychological, Romance, Sci-Fi, Slice of Life, Sports, Supernatural, Thriller
                     `)
             }
 
-            if (mangaMatch != null && lastMessage.messages[0].author.uid != myProfile.account.uid) {
-                console.log("buscando manga");
-                let res = await anilist.getManga(mangaMatch[1])
+            /* orden random manga */
+
+            if (randomMangaMatch != null && lastMessage.messages[0].author.uid != myProfile.account.uid) {
+                console.log("buscando una sugerencia de manga...");
+                let res = await anilist.getRandomManga(randomMangaMatch[1])
                 let textFixed = res
                     .description
                     .replace(/<br>\\*/g, `
@@ -111,9 +123,11 @@ Volumenes: ${res.volumes}
 
             }
 
-            if (animeMatch != null && lastMessage.messages[0].author.uid != myProfile.account.uid) {
-                console.log("buscando anime");
-                let res = await anilist.getAnime(animeMatch[1])
+            /* orden random anime */
+
+            if (randomAnimeMatch != null && lastMessage.messages[0].author.uid != myProfile.account.uid) {
+                console.log("buscando una sugerencia de anime...");
+                let res = await anilist.getRandomAnime(randomAnimeMatch[1])
                 let textFixed = res
                     .description
                     .replace(/<br>\\*/g, `
@@ -138,6 +152,123 @@ Volumenes: ${res.volumes}
                     });
                     
                     /*linea 63*/
+                    await amino.sendChat(
+                        auth.amino.community,
+                        receiver,
+                        `
+                        Nombre: ${res.title.native}
+Romaji: ${res.title.romaji}
+
+Inicio: ${res.startDate.day}-${res.startDate.month}-${res.startDate.year}
+Final: ${res.endDate.day}-${res.endDate.month}-${res.endDate.year}
+
+Episodios: ${res.episodes}
+Duración: ${res.duration} minutos
+                        `
+                    )
+
+                    await languageTranslator.translate(paramsTranslator, function (err, res) {
+                        const es_description = res
+                            .translations[0]
+                            .translation
+                        if (err) {
+                            console.log("error" + err)
+                        } else {
+                            (async function () {
+                                await amino.sendChat(auth.amino.community, receiver, es_description)
+                            })();
+                        }
+                    });
+            }
+
+            /* orden para buscar manga */
+
+            if (mangaMatch != null && lastMessage.messages[0].author.uid != myProfile.account.uid) {
+                console.log("buscando manga...");
+                let res = await anilist.getManga(mangaMatch[1])
+                let textFixed = res
+                    .description
+                    .replace(/<br>\\*/g, `
+                    `)
+
+                const paramsTranslator = {
+                    text: textFixed,
+                    model_id: 'en-es'
+                };
+
+                fetch(res.coverImage.large)
+                    .then(res => {
+                        const dest = fs.createWriteStream(`${timestamp}.jpg`);
+                        res.body.pipe(dest);
+                        dest.on("finish", async function(){
+                            await amino.sendImage(
+                                auth.amino.community,
+                                receiver,
+                                './' + timestamp + '.jpg'
+                            )
+                        })
+                    });
+                
+                /*linea 89*/
+                await amino.sendChat(
+                    auth.amino.community,
+                    receiver,
+                    `
+                    Nombre: ${res.title.native}
+Romaji: ${res.title.romaji}
+
+Inicio: ${res.startDate.day}-${res.startDate.month}-${res.startDate.year}
+Final: ${res.endDate.day}-${res.endDate.month}-${res.endDate.year}
+
+Capítulos: ${res.chapters}
+Volumenes: ${res.volumes}
+                    `
+                )
+
+                await languageTranslator.translate(paramsTranslator, function (err, res) {
+                    const es_description = res
+                        .translations[0]
+                        .translation
+                    if (err) {
+                        console.log("error" + err)
+                    } else {
+                        (async function () {
+                            await amino.sendChat(auth.amino.community, receiver, es_description)
+                        })();
+                    }
+                });
+
+            }
+
+            /* orden para buscar anime */
+
+            if (animeMatch != null && lastMessage.messages[0].author.uid != myProfile.account.uid) {
+                console.log("buscando anime...");
+                let res = await anilist.getAnime(animeMatch[1])
+                let textFixed = res
+                    .description
+                    .replace(/<br>\\*/g, `
+                    `)
+
+                const paramsTranslator = {
+                    text: textFixed,
+                    model_id: 'en-es'
+                };
+
+                fetch(res.coverImage.large)
+                    .then(res => {
+                        const dest = fs.createWriteStream(`${timestamp}.jpg`);
+                        res.body.pipe(dest);
+                        dest.on("finish", async function(){
+                            await amino.sendImage(
+                                auth.amino.community,
+                                receiver,
+                                './' + timestamp + '.jpg'
+                            )
+                        })
+                    });
+                    
+                    /*linea 89*/
                     await amino.sendChat(
                         auth.amino.community,
                         receiver,
--- helpers/anilist.js
+++ helpers/anilist.js
@@ -1,6 +1,84 @@
 const { request } = require("graphql-request")
 const endpoint = "https://graphql.anilist.co"
 
+async function getRandomManga(data){
+    const query = `{
+        Page {
+            media(genre: "${data}", type: MANGA, format: MANGA, isAdult: false) {
+                title {
+                    romaji
+                    native
+                }
+                coverImage {
+                    medium
+                    large
+                }
+                description
+                format
+                type
+                averageScore
+                chapters
+                volumes
+                startDate {
+                    year
+                    month
+                    day
+                }
+                endDate {
+                    year
+                    month
+                    day
+                }
+            }
+        }
+    }`
+
+    const res = await request(endpoint, query)
+    const keys = Object.keys(res.Page.media)
+
+    return res.Page.media[keys[ keys.length * Math.random() << 0]];
+}
+getRandomManga().catch(error => {return error})
+
+async function getRandomAnime(data){
+    const query = `{
+        Page {
+            media(genre: "${data}", type: ANIME, format: TV, isAdult: false) {
+                title {
+                    romaji
+                    native
+                }
+                coverImage {
+                    medium
+                    large
+                }
+                description
+                format
+                type
+                averageScore
+                episodes
+                duration
+                startDate {
+                    year
+                    month
+                    day
+                }
+                endDate {
+                    year
+                    month
+                    day
+                }
+            }
+        }
+    }`
+
+    const res = await request(endpoint, query)
+    const keys = Object.keys(res.Page.media)
+
+    return res.Page.media[keys[ keys.length * Math.random() << 0]];
+}
+getRandomAnime().catch(error => {return error})
+
 async function getManga(data){
     const query = `{
         Page {
@@ -79,5 +157,7 @@ getAnime().catch(error => {return error})
 
 module.exports = {
     getManga: getManga,
-    getAnime: getAnime
+    getAnime: getAnime,
+    getRandomAnime: getRandomAnime,
+    getRandomManga: getRandomManga
 }
